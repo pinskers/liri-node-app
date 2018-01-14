8a4872efb06d0bcf657b2928e29e46b60ac2c050
Michelle Pinsky <michellepinsky@unboxedtechnology.com>
2018-01-14 10:16:36 -0500

    Building out more detail in notes so I can figure out what to do later.

--- liri.js
+++ liri.js
@@ -18,19 +18,44 @@ var keys = require("keys");
 // LOGIC
 switch(command){
   case: "my-tweets";
-  // probably a function that shows tweets here
+  // show your last 20 tweets and when they were created at in your terminal/bash window
   break;
 
   case: "spotify-this-song";
-  // probably a function that shows songs here
+  // show the following information about the song in your terminal/bash window
+     
+  //   * Artist(s)
+     
+  //   * The song's name
+  
+  //   * A preview link of the song from Spotify
+  
+  //   * The album that the song is from
+
+  //   * If no song is provided then your program will default to "The Sign" by Ace of Base.
   break;
 
   case: "movie-this";
-  // probably a function showing omdb titles here
-  break;
+  //    output the following information to your terminal/bash window:
+
+  //     * Title of the movie.
+  //     * Year the movie came out.
+  //     * IMDB Rating of the movie.
+  //     * Rotten Tomatoes Rating of the movie.
+  //     * Country where the movie was produced.
+  //     * Language of the movie.
+  //     * Plot of the movie.
+  //     * Actors in the movie.
+
+  //   * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+  //   break;
 
   case: "do-what-it-says";
-  // not sure what this will do yet
+  // Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+     
+  //    * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+     
+  //    * Feel free to change the text in that document to test out the feature for other commands.
   break;
 
   default:
