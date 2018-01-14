4583a75ab951122968ccaaeffe366aa71d976fd9
Michelle Pinsky <michellepinsky@unboxedtechnology.com>
2018-01-14 10:10:56 -0500

    Beginning to build out logic.

--- keys.js
+++ keys.js
@@ -1,11 +1,10 @@
-js
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.X8qOOMqFh67Yh0OASSm04wZYG,
+  consumer_secret: process.env.77VMgDLi3VWgyQsk0RTZq04eBOE8M9UIuhGY7B2HOCQNctRQSc,
+  access_token_key: process.env.952253267580506117-mndPG7oadiBinrrGlc1O27xrb1IvN6W,
+  access_token_secret: process.env.Rty0BbsxLHfdbYDAKTCF0xVSdew8YtU5V3yWjgwjl9hFr
 };
 
 exports.spotify = {
--- liri.js
+++ liri.js
@@ -1,8 +1,39 @@
-js
-require("dotenv").config();
+// VARIABLES
 
+// DotEnv Node Package
+var dotenv = require('dotenv').config()
+
+// Twitter Node Package
+var Twitter = require('twitter');
+
+// Spotify Node Package
+var Spotify = require('node-spotify-api');
+
+// Request Node Package
+var request = require('request');
+
+// Key linkup
 var keys = require("keys");
 
-js
-  var spotify = new Spotify(keys.spotify);
-  var client = new Twitter(keys.twitter);
+// LOGIC
+switch(command){
+  case: "my-tweets";
+  // probably a function that shows tweets here
+  break;
+
+  case: "spotify-this-song";
+  // probably a function that shows songs here
+  break;
+
+  case: "movie-this";
+  // probably a function showing omdb titles here
+  break;
+
+  case: "do-what-it-says";
+  // not sure what this will do yet
+  break;
+
+  default:
+  console.log ("Please enter one of the following commands:\nmy-tweets\nspotify-this-song\nmovie-this\ndo-what-it-says");
+  break;
+}
